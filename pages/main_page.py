618460d8e24502c3f24285ba857e203f245ea42c
Анастасия Туголукова <tugolukova_a00@mail.ru>
2026-03-06 06:25:25 +0000

    Обращения к драйверу перенесены в base_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -25,12 +25,12 @@ class MainPage(BasePage):
     @allure.step('Нажимаем на  {button_name}')
     def click_order_button(self, locator, button_name):
         element = self.wait_for_clickable(locator, timeout=10)
-        self.driver.execute_script("arguments[0].click();", element)
+        self.click_with_js(element)
 
     @allure.step('Нажимаем на {question_name}')
     def click_faq_question(self, question_locator, question_name):
         element = self.wait_for_clickable(question_locator, timeout=10)
-        self.driver.execute_script("arguments[0].click();", element)
+        self.click_with_js(element)
 
     @allure.step('Нажимаем на логотип "Самокат"')
     def click_scooter_logo(self):
